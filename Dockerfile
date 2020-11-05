552d0675082ca43d56d6350b60ee303d29a5c877
Matthew Wild <mwild1@gmail.com>
2020-11-05 15:41:18 +0000

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,21 @@
+FROM debian:buster-slim
+
+ARG BUILD_SERIES=dev
+ARG BUILD_ID=0
+
+VOLUME ["/snikket"]
+
+ENTRYPOINT ["/usr/bin/tini"]
+CMD ["/bin/sh", "/entrypoint.sh"]
+
+RUN apt-get update \
+    && DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends \
+        certbot tini anacron \
+    && rm -rf /var/lib/apt/lists/* \
+    && apt-get autoremove -y \
+    && rm -rf /var/cache/*
+
+ADD entrypoint.sh /entrypoint.sh
+ADD certbot.cron /etc/cron.daily/certbot
+RUN chmod 555 /etc/cron.daily/certbot
+RUN useradd -md /snikket/letsencrypt letsencrypt
---
+++ entrypoint.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+PUID=${PUID:=$(stat -c %u /snikket)}
+PGID=${PGID:=$(stat -c %g /snikket)}
+
+if [ "$PUID" != 0 ] && [ "$PGID" != 0 ]; then
+        usermod  -o -u "$PUID" letsencrypt
+        groupmod -o -g "$PGID" letsencrypt
+fi
+
+if ! test -d /snikket/letsencrypt; then
+        install -o letsencrypt -g letsencrypt -m 750 -d /snikket/letsencrypt;
+fi
+
+install -o letsencrypt -g letsencrypt -m 750 -d /var/lib/letsencrypt;
+install -o letsencrypt -g letsencrypt -m 750 -d /var/log/letsencrypt;
+install -o letsencrypt -g letsencrypt -m 755 -d /var/www/.well-known/acme-challenge;
+
+chown -R letsencrypt:letsencrypt /snikket/letsencrypt
+
+exec /bin/sh -c "/usr/sbin/anacron -d -n && sleep 3600"
